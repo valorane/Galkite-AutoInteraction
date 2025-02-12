0a0634355caf802be30e832dadc8964fb647849c
valorane <gbalsem088@gmail.com>
2025-02-12 16:13:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import ora from 'ora';
 import fs from 'fs';
 
 async function main(numberOfInteractions) {
-  cfonts.say('NT Exhaust', {
+  cfonts.say('END AIRDROP', {
     font: 'block',
     align: 'center',
     colors: ['cyan', 'magenta'],
